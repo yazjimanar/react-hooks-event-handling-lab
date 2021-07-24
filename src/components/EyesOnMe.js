d76fff8cf45cb0198aa02dd61044da9318e77ebb
yazjimanar <m.gmail.com>
2021-07-24 14:45:29 +0300

    hi

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function EyesOnMe() {
+  function focus() {
+    console.log("Good!");
+  }
+  function blur() {
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <div>
+      <button onFocus={() => focus()} onBlur={() => blur()}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+
+import React from "react";
+
+function Keypad() {
+  function handleChange(event) {
+    console.log("Entering password...");
+  }
+
+  return (
+    <form onChange={handleChange}>
+      <input type="password" name="password" placeholder="Enter password..." />
+    </form>
+  );
+}
+export default Keypad;
